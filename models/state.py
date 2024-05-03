d83a5d3efdd68a4f110a3d5944485512f503ef28
baragu <baraguantonyy@gmail.com>
2024-05-04 01:20:09 +0300

    update engine

--- models/state.py
+++ models/state.py
@@ -12,8 +12,10 @@ class State(BaseModel, Base):
     """Representation of state """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="states")
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
     else:
         name = ""
 
